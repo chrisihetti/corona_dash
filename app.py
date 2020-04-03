bc9ff77e649823d4cd9d2a98f614284ace1b7dbf
Christoph Hettegger <christoph.hettegger@gmx.at>
2020-04-03 10:42:40 +0200

    First modifications

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ colors = {
 data = pd.read_csv("cleaned_data.csv")
 data['date'] = pd.to_datetime(data['date'], format='%d.%m.%Y')
 casesAusria = go.Scatter(
-
     x=data.date,
     y=data.cases,
     name="Austria",
     line=dict(color='#39ff14'),
+    mode='lines+markers',
     opacity=0.8)
 
 data = [casesAusria]
@@ -33,10 +33,6 @@ layout = dict(
                      label='1m',
                      step='month',
                      stepmode='backward'),
-                dict(count=6,
-                     label='6m',
-                     step='month',
-                     stepmode='backward'),
                 dict(step='all')
             ])
         ),
@@ -44,15 +40,27 @@ layout = dict(
         type='date'
     ),
     paper_bgcolor=colors['background'],
-    plot_bgcolor=colors['background']
+    plot_bgcolor=colors['background'],
+    title={
+        'text': "Corona Cases in Austria",
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis_title="Date",
+    yaxis_title="Confirmed Cases",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color=colors['text']
+    )
 )
 
 fig = dict(data=data, layout=layout)
 
 # Now here's the Dash part:
 
-app.layout = html.Div([
-    html.H1('Dash Introduction'),
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     dcc.Graph(id='my-graph', figure=fig)
 ])
 
